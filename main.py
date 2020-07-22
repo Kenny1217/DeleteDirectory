d34d1d823d415b8df4d781f0dde07c3f5b773b1a
Kenny Morris <kmorris1217@gmail.com>
2020-07-22 18:32:49 -0400

    Added ability to recursively check for files/folders.

--- main.py
+++ main.py
@@ -1,5 +1,20 @@
 import os
-import stat
+
+
+# This function recursively checks and deletes files/folders in the directory
+def check(dir_path):
+    file_list = os.listdir(dir_path)  # Create list of files/folders in directory
+    for x in range(len(file_list)):
+        temp = dir_path + "/" + file_list[x]  # Current file/folder selected
+        if os.path.isfile(temp):  # Check if item is a file
+            print("Delete File")  # Delete file
+        if os.path.isdir(temp):  # Check if item is a folder
+            directory = os.listdir(temp)
+            if len(directory) == 0:  # Check if folder is empty
+                print("Delete Folder")  # Delete Folder
+            else:
+                check(temp)
+                print("Delete Folder")  # Delete Folder
 
 
 def main():
