90d8f227df19908725b3004aaf177c6e581f22f6
Kenny Morris <kmorris1217@gmail.com>
2020-07-22 18:48:11 -0400

    Added ability to delete files/folders.

--- main.py
+++ main.py
@@ -1,25 +1,38 @@
 import os
 
 
+# This function deletes a file
+def delete_file(dir_path):
+    os.remove(dir_path)
+    print("Deleted " + dir_path)
+
+
+# This function deletes an empty folder
+def delete_folder(dir_path):
+    os.rmdir(dir_path)  # Delete folder
+    print("Deleted " + dir_path)
+
+
 # This function recursively checks and deletes files/folders in the directory
 def check(dir_path):
     file_list = os.listdir(dir_path)  # Create list of files/folders in directory
     for x in range(len(file_list)):
         temp = dir_path + "/" + file_list[x]  # Current file/folder selected
         if os.path.isfile(temp):  # Check if item is a file
-            print("Delete File")  # Delete file
+            delete_file(temp)  # Delete file
         if os.path.isdir(temp):  # Check if item is a folder
             directory = os.listdir(temp)
             if len(directory) == 0:  # Check if folder is empty
-                print("Delete Folder")  # Delete Folder
+                delete_folder(temp)  # Delete Folder
             else:
                 check(temp)
-                print("Delete Folder")  # Delete Folder
+                delete_folder(temp)  # Delete Folder
+    delete_folder(dir_path)  # Delete Folder
 
 
 def main():
     start_path = input("Enter Dir: ")  # The directory the user wants to delete
-    print(start_path)
+    check(start_path)  # Delete everything in directory
 
 
 if __name__ == "__main__":
