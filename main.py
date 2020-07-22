a50faa291a9c0e4cd3977c1885cb04ac144f20f2
Kenny Morris <kmorris1217@gmail.com>
2020-07-22 17:28:24 -0400

    Added imports. Added prompt to enter directory.

---
+++ main.py
@@ -0,0 +1,11 @@
+import os
+import stat
+
+
+def main():
+    start_path = input("Enter Dir: ")  # The directory the user wants to delete
+    print(start_path)
+
+
+if __name__ == "__main__":
+    main()
